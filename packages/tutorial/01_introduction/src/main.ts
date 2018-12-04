efb9d9233d5f12e10054ba037a2fff4d2666daf0
Boris Kaul <localvoid@gmail.com>
2018-12-04 16:45:12 +0700

    chore: update

--- packages/tutorial/01_introduction/src/main.ts
+++ packages/tutorial/01_introduction/src/main.ts
@@ -1,6 +1,7 @@
-import { render, component, _ } from "ivi";
-import { div } from "ivi-html";
+import { _, render } from "ivi";
+import { h1 } from "ivi-html";
 
-const Hello = component<string>(() => (text) => div(_, _, `Hello ${text}!`));
-
-render(Hello("World"), document.getElementById("app")!);
+render(
+  h1(_, _, "Hello World!"),
+  document.getElementById("app")!,
+);
